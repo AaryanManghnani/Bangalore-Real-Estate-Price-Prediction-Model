20c96c09de9d04a2fde62ccf875d89c719ebd462
AaryanManghnani <aaryanmanghnani1@gmail.com>
2023-12-11 02:17:45 +0530

    small commit 2

--- client/app.js
+++ client/app.js
@@ -5,7 +5,7 @@ function getBathValue() {
           return parseInt(i)+1;
       }
     }
-    return -1; // Invalid Value
+    return -1; 
   }
   
   function getBHKValue() {
@@ -15,7 +15,7 @@ function getBathValue() {
           return parseInt(i)+1;
       }
     }
-    return -1; // Invalid Value
+    return -1; 
   }
   
   function onClickedEstimatePrice() {
@@ -26,8 +26,7 @@ function getBathValue() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
   
-    var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-    //var url = "/api/predict_home_price"; 
+    var url = "http://127.0.0.1:5000/predict_home_price"; 
   
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
@@ -44,7 +43,7 @@ function getBathValue() {
   function onPageLoad() {
     console.log( "document loaded" );
     var url = "http://127.0.0.1:5000/get_location_names"; 
-    //var url = "/api/get_location_names"; // Use this if  you are using nginx.
+
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
         if(data) {
